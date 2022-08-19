43fcfd368885330e317e60935561f9db384fc034
tsukasa-tazawa <tsukasa.t@aisaac.jp>
2022-08-19 11:43:40 +0900

    add/todo-list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    def index
+      render html: "TODOアプリにようこそ"
+    end
+
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def tsukasa
+    render 'ようこそ'
+  end
+end
