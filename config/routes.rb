11ad44a00a30005d599041b652821003c0163b2a
tsukasa-tazawa <tsukasa.t@aisaac.jp>
2022-10-19 17:46:19 +0900

    ログインページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'static_pages/home'
   root 'application#index'
   get  '/users/new',  to: 'users#new'
+  get  '/login',   to: 'sessions#new'
   resources :tasks
   resources :users
 end
