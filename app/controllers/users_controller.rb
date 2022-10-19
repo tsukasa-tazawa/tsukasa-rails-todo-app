d56f66a10617f0bcf6ea5cb606cefc7578b16271
tsukasa-tazawa <tsukasa.t@aisaac.jp>
2022-10-19 17:12:39 +0900

    トップページとユーザー登録ページの遷移

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def tsukasa
     render 'ようこそ'
   end
+
+  def new
+    @user = User.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   root 'application#index'
-  get  '/signup',  to: 'users#new'
+  get  '/users/new',  to: 'users#new'
   resources :tasks
+  resources :users
 end
