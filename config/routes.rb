0e1ec9be20db17c7906a45a668e99a19517566de
tsukasa-tazawa <tsukasa.t@aisaac.jp>
2022-10-20 14:25:34 +0900

    ユーザー登録処理

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'static_pages/home'
   root 'application#index'
+  get  '/users', to: 'users#index'
+  get  '/users', to: 'users#show'
   get  '/users/new',  to: 'users#new'
+  post '/users',  to: 'users#create'
   get  '/login',   to: 'sessions#new'
   resources :tasks
   resources :users
