a115a9f8edf3436433507dc09a46bb4a6fabb3fa
tsukasa-tazawa <tsukasa.t@aisaac.jp>
2022-10-21 10:51:21 +0900

    登録したユーザーがログインできるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    include SessionsHelper
 
     def index
       render html: "TODOアプリにようこそ"
     end
 
+
+    private
+   # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        redirect_to login_url
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'static_pages/home'
   root 'application#index'
-  get  '/users', to: 'users#index'
-  get  '/users', to: 'users#show'
+  get  '/users',   to: 'users#index'
+  get  '/users',   to: 'users#show'
   get  '/users/new',  to: 'users#new'
   post '/users',  to: 'users#create'
-  get  '/login',   to: 'sessions#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
   resources :tasks
   resources :users
 end
