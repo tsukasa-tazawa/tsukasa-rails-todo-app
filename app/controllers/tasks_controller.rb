8fb5c3e6d474cc371169d4d57b23ed299955b603
tsukasa-tazawa <tsukasa.t@aisaac.jp>
2022-10-25 17:23:11 +0900

    ステータスの分類

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -37,6 +37,6 @@ class TasksController < ApplicationController
 
     private
         def task_params
-          params.require(:task).permit(:title, :content)
+          params.require(:task).permit(:title, :content, :status)
         end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,6 @@ class Task < ApplicationRecord
   belongs_to:user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
+
+  enum status: {"未着手":0, "進行中":1, "完了":2}
 end
