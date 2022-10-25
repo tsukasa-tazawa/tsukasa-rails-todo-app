a1534cf11ccf9b0328d0d9efacbaadffc10a9c53
tsukasa-tazawa <tsukasa.t@aisaac.jp>
2022-10-25 14:32:27 +0900

    delete git commit -m delete quit

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'static_pages/home'
   root 'application#index'
-  get  '/users',   to: 'users#index'
-  get  '/users',   to: 'users#show'
-  get  '/users/new',  to: 'users#new'
-  post '/users',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   resources :tasks
